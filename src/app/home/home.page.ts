559f8487b6905fbdee3a190d306cf36fe05b87c5
SaschaPWittwer <sascha.wittwer@bluewin.ch>
2019-06-10 12:03:25 +0200

    Add ionic project template

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  constructor(private geolocation: Geolocation) { }
+
+  ngOnInit(): void {
+    this.test();
+  }
+
+  private async test(): Promise<void> {
+
+    let pos = await this.geolocation.getCurrentPosition();
+    console.log(pos.coords.latitude, pos.coords.longitude);
+  }
+
+}
